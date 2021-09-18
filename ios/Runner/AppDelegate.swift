e59d57c4e86a001cb92747ed63dca2367546a40f
Apple Tech <appletech@Apples-MacBook-Pro.local>
2021-09-18 23:04:08 +0500

    add new changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,6 +10,11 @@ import GoogleMaps
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyC-ADl5riVsKPU8F9qe1jOLVQwGSdh_bOo")
     GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+
